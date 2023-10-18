1de062801bd551748b6c23221c999d60d290c59b
Meng-Xin <li997266754@gmail.com>
2023-10-18 10:03:40 +0800

    feat: UnaryRPC(一元RPC)实现案例

---
+++ grpc/UnaryRPC/client/client.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"UnaryRPC/protoc/pb"
+	"context"
+	"fmt"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"log/slog"
+)
+
+func main() {
+	conn, err := grpc.Dial("127.0.0.1:7009", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+	defer conn.Close()
+	client := pb.NewWeatherServiceClient(conn)
+	bjData, err := client.GetTodayData(context.Background(), &pb.WeatherReq{City: "bj"})
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+	fmt.Println(bjData.GetData())
+
+	shData, err := client.GetTodayData(context.Background(), &pb.WeatherReq{City: "sh"})
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+	fmt.Println(shData.GetData())
+
+	hzData, err := client.GetTodayData(context.Background(), &pb.WeatherReq{City: "hz"})
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+	fmt.Println(hzData.GetData())
+
+	szData, err := client.GetTodayData(context.Background(), &pb.WeatherReq{City: "sz"})
+	if err != nil {
+		slog.Error(err.Error())
+		return
+	}
+	fmt.Println(szData.GetData())
+}
